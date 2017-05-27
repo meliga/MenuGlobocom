d3df605117aa664519293ce1e2d8a68f78ad6d65
diedromeliga <diedromeliga@gmail.com>
2017-05-27 12:34:06 -0300

    Configurações iniciais do Gulp

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+
+// Task Padrão - Executar com o comando 'gulp'
+// Default task - Run with command 'gulp'
+
+gulp.task('default', function () {
+    console.log('Gulp funcionando... ');
+});
